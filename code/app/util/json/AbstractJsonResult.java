f073955c53e4a294d884b3984d9f1f93cfa917c7
brecht <brecht_de_meulenaere@outlook.com>
2016-09-08 15:18:13 +0200

    Init files for json results

---
+++ code/app/util/json/AbstractJsonResult.java
@@ -0,0 +1,7 @@
+package util.json;
+
+/**
+ * Created by brecht on 9/8/16.
+ */
+public abstract class AbstractJsonResult {
+}
---
+++ code/app/util/json/DataJsonResult.java
@@ -0,0 +1,7 @@
+package util.json;
+
+/**
+ * Created by brecht on 9/8/16.
+ */
+public class DataJsonResult extends AbstractJsonResult {
+}
