83745ceb3bbb025b98e68d7a8d87750f4ef56dd9
brecht <brecht_de_meulenaere@outlook.com>
2016-09-09 00:21:55 +0200

    Add fetchFormData

---
+++ code/app/util/json/InvalidJsonException.java
@@ -0,0 +1,11 @@
+package util.json;
+
+public class InvalidJsonException extends Exception {
+    public InvalidJsonException(String message) {
+        super(message);
+    }
+
+    public InvalidJsonException() {
+
+    }
+}
