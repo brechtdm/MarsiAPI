28184bc38504c4935a6a43da252f2e381b94dd74
brecht <brecht_de_meulenaere@outlook.com>
2016-09-08 22:59:51 +0200

    Create TokenJsonResult

---
+++ code/app/util/json/TokenJsonResult.java
@@ -0,0 +1,24 @@
+package util.json;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+/**
+ * Created by brecht on 9/8/16.
+ */
+public class TokenJsonResult extends AbstractJsonResult {
+
+    private String token;
+
+    public TokenJsonResult(int code, String token) {
+        super(code);
+        this.token = token;
+    }
+
+    @Override
+    public ObjectNode toJson() {
+        ObjectNode resultObject = super.toJson();
+        resultObject.put("token", token);
+
+        return resultObject;
+    }
+}
