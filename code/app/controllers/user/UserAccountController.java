c15f36558fb01f46d25ee76599a15841f875bf77
brecht <brecht_de_meulenaere@outlook.com>
2016-09-09 00:53:21 +0200

    Created controller

---
+++ code/app/controllers/user/UserAccountController.java
@@ -0,0 +1,16 @@
+package controllers.user;
+
+public class UserAccountController {
+    // Get
+    //  - id
+    //  - me
+    // Create
+    // Delete
+
+    // Update password
+    // Update username
+    // Update email
+
+    // Validate registrationKey
+    // Resend registrationKey
+}
