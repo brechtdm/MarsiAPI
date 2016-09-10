93527d326eca01bdc7009343dae7a69943efe237
brecht <brecht_de_meulenaere@outlook.com>
2016-09-10 21:26:58 +0200

    add QueryHelper

---
+++ code/app/util/QueryHelper.java
@@ -0,0 +1,81 @@
+package util;
+
+import com.avaje.ebean.ExpressionList;
+import com.avaje.ebean.Model;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.Logger;
+import play.libs.Json;
+
+import static play.mvc.Controller.request;
+
+/**
+ * Created by matthias on 18/03/2015.
+ */
+public class QueryHelper {
+
+    private static final String PAGE_SIZE = "pageSize";
+    private static final String ORDER = "order";
+
+    public static <T extends Model> ExpressionList<T> buildQuery(Class<T> clazz , ExpressionList<T> exp) {
+        return buildQuery(clazz,exp,false);
+    }
+
+    public static <T extends Model> ExpressionList<T> buildQuery(Class<T> clazz , ExpressionList<T> exp, boolean ignorePage) {
+        JsonNode model;
+        try {
+            model = Json.toJson(clazz.newInstance());
+        } catch (InstantiationException | IllegalAccessException e) {
+            play.Logger.error("the provided class is not accessible or cannot be instantiated", e);
+            return null;
+        }
+
+        int pageSize = getPageSize();
+        boolean asc = isAsc();
+
+        request().queryString().entrySet().stream().forEach(e -> {
+            switch (e.getKey()) {
+                case "orderBy" :
+                    exp.order().asc(e.getValue()[0]);
+                    if (!asc)
+                        exp.order().reverse();
+                    break;
+                case "page" :
+                    if (pageSize > 0 && !ignorePage)
+                        exp.setFirstRow(pageSize * Integer.parseInt(e.getValue()[0])).setMaxRows(pageSize);
+                    break;
+                case "total" :
+                    break;
+                case "searchOn":
+                default :
+                    if (model.findValue(e.getKey()) != null && e.getValue()[0].length() != 0) {
+                        try {
+                            Class fieldType = clazz.getDeclaredField(e.getKey()).getType();
+                            String value = e.getValue()[0];
+                            if (fieldType == String.class)
+                                exp.contains(e.getKey(), value);
+                            else
+                                exp.eq(e.getKey(), value);
+                        } catch (NoSuchFieldException e1) {
+                            Logger.info("QueryHelper", e1);
+                            // ignore unknown properties
+                        }
+                    }
+            }
+        });
+
+        return exp;
+    }
+
+    private static int getPageSize() {
+        if (request().queryString().containsKey(PAGE_SIZE))
+            return Integer.parseInt(request().queryString().get(PAGE_SIZE)[0]);
+        return -1;
+    }
+
+    private static boolean isAsc() {
+        if (request().queryString().containsKey(ORDER))
+            return !"desc".equals(request().queryString().get(ORDER)[0]);
+        return true;
+    }
+}
+
